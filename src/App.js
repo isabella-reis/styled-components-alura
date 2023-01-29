6f6c980339115673cf87f4610c07628e2465ca4f
Isabella Reis <isabellanery15@hotmail.com>
2023-01-29 15:03:26 -0300

    Estilos refatorados

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
+import { GlobalStyle } from "./Components/globalStyle";
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Cabecalho />
       <Container />
     </>
--- src/Components/Titulo/index.jsx
+++ src/Components/Titulo/index.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import styled from "styled-components";
 
-const Titulo = ({ children }) => {
-  return <h1 className="titulo">{children}</h1>;
-};
-export default Titulo;
+export const Titulo = styled.h1`
+  color: grey;
+  padding: 25px;
+`;
+
+// --> Não precisamos mais utilizar esse componente, pois o título será um componente APENAS estilizado  <--
+// --> Já fica entendido que a tag h1 receberá um conteúdo vindo de um children de um componente pai  <--
+// --> A tag está sendo chamada lá no Container <--
+
+// const Titulo = ({ children }) => {
+//   return <h1 className="titulo">{children}</h1>;
+// };
+// export default Titulo;
